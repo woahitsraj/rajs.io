7223544d39d7178d53b2ebc4b24aa4b4fd29f11e
Rajan Singh <rajan1992@hotmail.com>
2026-04-07 18:08:44 +0200

    Fix localization

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
-			strategy: ['url', 'cookie', 'baseLocale']
+			strategy: ['cookie', 'preferredLanguage', 'url', 'baseLocale']
 		}),
 		sveltekit()
 	],
