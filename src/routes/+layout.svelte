acd8d0975a18c2582c63d47fba2e1a1992821ef4
Rajan Singh <rajan1992@hotmail.com>
2026-04-30 17:18:41 +0200

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 	import '../app.css';
 	import NavigationTransitions from '$lib/NavigationTransitions.svelte';
 	interface Props {
 		children?: import('svelte').Snippet;
 	}
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	let { children }: Props = $props();
 </script>
